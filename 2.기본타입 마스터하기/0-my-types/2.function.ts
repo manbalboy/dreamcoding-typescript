6bef6db9e4e4d930fc16bef151aae8a19c740259
manbalboy <manbalboy@hanmail.net>
2022-05-15 21:30:51 +0900

    함수 타입 이용

--- 2.기본타입 마스터하기/0-my-types/2.function.ts
+++ 2.기본타입 마스터하기/0-my-types/2.function.ts
@@ -1,28 +1,53 @@
 {
-  function jsAdd(num1, num2) {
-    return num1 + num2;
+  // function jsAdd(num1, num2) {
+  //   return num1 + num2;
+  // }
+
+  // function typeScriptFn(num1: number, num2: number): number {
+  //   return num1 + num2;
+  // }
+
+  // function jsFetchNum(id) {
+  //   //code ...
+  //   //code ...
+  //   //code ...
+
+  //   return new Promise((resolve, reject) => {
+  //     resolve(100);
+  //   });
+  // }
+
+  // function typeFetchNum(id: string): Promise<number> {
+  //   //code ...
+  //   //code ...
+  //   //code ...
+  //   return new Promise((resolve, reject) => {
+  //     resolve(100);
+  //   });
+  // }
+
+  //Javascript => TypeScript
+  // Optional parameter
+  function printName(firstName: string, lastName?: string) {
+    console.log(firstName);
+    console.log(lastName);
   }
 
-  function typeScriptFn(num1: number, num2: number): number {
-    return num1 + num2;
+  printName("hun", "jung");
+  printName("yun");
+  printName("ha", undefined);
+
+  //default parameter
+  function printMessage(message = "default message") {
+    console.log(message);
   }
 
-  function jsFetchNum(id) {
-    //code ...
-    //code ...
-    //code ...
+  printMessage();
 
-    return new Promise((resolve, reject) => {
-      resolve(100);
-    });
+  //Rest parameter
+  function addNumbers(...numbers: number[]): number {
+    return numbers.reduce((a, b) => a + b, 0);
   }
 
-  function typeFetchNum(id: string): Promise<number> {
-    //code ...
-    //code ...
-    //code ...
-    return new Promise((resolve, reject) => {
-      resolve(100);
-    });
-  }
+  console.log(addNumbers(1, 3, 4));
 }
